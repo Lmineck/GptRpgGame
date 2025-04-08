c749fc0c081317a4e35f6c76e193f5e37c4499ff
MINHYEOK LEE <imhfox3573@it-cous.com>
2025-04-08 15:26:32 +0900

    git init

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",  // 반드시 jsx 포함!
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
